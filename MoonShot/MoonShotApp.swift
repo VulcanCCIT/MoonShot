27981ae7b6ca5788b1e95a7e3bf4537a6f2e7e90
VulcanCCIT <chuckcondron@chuckcondron.com>
2023-06-23 10:31:46 -0500

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Chuck Condron on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
