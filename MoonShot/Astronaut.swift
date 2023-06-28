180296454fd34ed47970fc3713840dfd02b48db5
VulcanCCIT <chuckcondron@chuckcondron.com>
2023-06-27 19:31:24 -0500

    First Commit
    App to demonstrate multiple views and merging JSON

---
+++ MoonShot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  MoonShot
+//
+//  Created by Chuck Condron on 6/26/23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+  let id: String
+  let name: String
+  let description: String
+}
---
+++ MoonShot/Color-Theme.swift
@@ -0,0 +1,19 @@
+//
+//  Color-Theme.swift
+//  MoonShot
+//
+//  Created by Chuck Condron on 6/26/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension ShapeStyle where Self == Color {
+  static var darkBackground: Color {
+    Color(red: 0.1, green: 0.1, blue: 0.2)
+  }
+  
+  static var lightBackground: Color {
+    Color(red: 0.5, green: 0.5, blue: 0.6)
+  }
+}
